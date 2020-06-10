5b2f37680532f2617f72a21044ac267ffbb4c053
John Gilbert <johngilbert2571@gmail.com>
2020-06-10 17:50:25 +0800

    Small changes

--- experiment_cifar10.py
+++ experiment_cifar10.py
@@ -1,24 +1,26 @@
+# Imports
 import torch
-import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
-import torchvision
-import torchvision.transforms as transforms
-import torchvision.models as models
+import torchvision as tv
 
 from NestedLookahead import NestedLookahead
 
 import os
 import time
-from matplotlib import pyplot as plt
 import pandas as pd
-import numpy as np
 
 import argparse
-
 import gc
-gc.collect()
 
+gc.collect() # run garbage collection just in case
+
+# Settings
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+root_path = './cifar_data'
+verbose = True
+loss_func = torch.nn.CrossEntropyLoss()
+
+# Command Line Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--epochs", help="number of epochs", type=int, default=30)
 parser.add_argument("--lr", help="learning rate", type=float, default=0.001)
@@ -34,7 +36,7 @@ parser.add_argument("--h", help="outer slow step size (nested lookahead)", type=
 parser.add_argument("--pullback", help="pullback type (nested lookahead)", type=str, default="None", 
     choices=["None", "reset", "pullback", "reset_inner", "reset_outer", "pullback_inner", "pullback_outer"])
 parser.add_argument("--tag", help="tag for result csv", default=0)
-#parser.add_argument("--verbose", help="prints loss and accuracy", type=bool, default=True)
+#parser.add_argument("--verbose", help="prints loss and accuracy", default=True)
 args = parser.parse_args()
 
 # Parameters
@@ -45,6 +47,7 @@ args = parser.parse_args()
 #weight_decay = 1e-3
 #opt_t = 'Adam' # optimizer type
 
+# Parameters
 epochs = args.epochs
 bs = args.bs
 lr = args.lr
@@ -62,45 +65,48 @@ assert opt_t in ['SGD', 'Adam','Lookahead_SGD','Lookahead_Adam','NestedLookahead
 # h=0.5 # outer slow step size
 # pullback=None
 
-k = args.k
-a = args.a
-s = args.s
-h = args.h
+# (Nested) Lookahead Parameters
+k = args.k # inner slow steps
+a = args.a # alpha; inner slow step size
+s = args.s # outer slow steps
+h = args.h # outer slow step size
 pullback = None if 'None' in args.pullback else args.pullback
 
+# If not nested, only use inner slow steps and fast steps (outer slow steps = 0)
 if (opt_t == 'Lookahead_SGD') or (opt_t == 'Lookahead_Adam'):
     s = 0
 
-loss_func = nn.CrossEntropyLoss()
+def to_device(*stuff):
+    "moves stuff to device (gpu)"
+    return [obj.to(device) for obj in stuff]
+    
+def to_cpu(*stuff):
+    "moves stuff to cpu"
+    return [obj.to('cpu') for obj in stuff]
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-root_path = './cifar_data'
-verbose = True
-
-def to_device(*args):
-    return [obj.to(device) for obj in args]
-
-# Transformations performed on data
-trans = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ColorJitter(brightness=0, contrast=(0.7,1.), saturation=(0.7,1.), hue=(0.,0.1)),
-    transforms.RandomRotation(45),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # mean and std taken from imagenet defaults
-#     transforms.RandomErasing(p=0.5, scale=(0.02,0.10), value=128)
+# Data Transformations
+trans = tv.transforms.Compose([
+    tv.transforms.RandomCrop(32, padding=4),
+    tv.transforms.RandomHorizontalFlip(),
+    tv.transforms.ColorJitter(brightness=0, contrast=(0.7,1.), saturation=(0.7,1.), hue=(0.,0.1)),
+    tv.transforms.RandomRotation(45),
+    tv.transforms.ToTensor(),
+    tv.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # mean and std taken from imagenet defaults
+#     tv.transforms.RandomErasing(p=0.5, scale=(0.02,0.10), value=128)
         ])
 
-train_ds = torchvision.datasets.CIFAR10(root=root_path, train=True, download=True, transform=trans)
+# Datasets and Dataloaders
+train_ds = tv.datasets.CIFAR10(root=root_path, train=True, download=True, transform=trans)
 train_dl = torch.utils.data.DataLoader(train_ds, batch_size=bs, shuffle=True)
 
-test_ds = torchvision.datasets.CIFAR10(root=root_path, train=False, download=True, transform=trans)
+test_ds = tv.datasets.CIFAR10(root=root_path, train=False, download=True, transform=trans)
 test_dl = torch.utils.data.DataLoader(test_ds, batch_size=bs, shuffle=False)
 
 # Model
-model = models.resnet18(pretrained=False)
+model = tv.models.resnet18(pretrained=False)
 model = model.to(device)
 
+# Set optimizer
 if opt_t == 'SGD':
     opt = optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
 
@@ -123,7 +129,7 @@ elif opt_t == 'NestedLookahead_Adam':
     inner_opt = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
     opt = NestedLookahead(inner_opt, k=k, a=a, s=s, h=h, pullback=pullback)
 
-# Training
+# Training Loop
 def train(model, train_dl, loss_func, opt):
     model.train()
     correct = total = train_loss = 0
@@ -136,12 +142,14 @@ def train(model, train_dl, loss_func, opt):
         loss.backward()
         opt.step()
         train_loss += loss.item()
-        _, Y_pred = Y_pred.max(1)
-        correct += (Y_pred == Y_true).type(torch.DoubleTensor).sum().item()
+        X, Y_true, Y_pred = to_cpu(X, Y_true, Y_pred)
+        _, Y_pred = Y_pred.max(dim=1)
+        correct += (Y_pred == Y_true).type(torch.IntTensor).sum().item()
         total += len(Y_true)
         accs.append(correct/total)
     return accs
 
+# Validation / Test Loop
 def validate(model, test_dl, loss_func):
     model.eval()
     correct = total = test_loss = 0
@@ -152,8 +160,9 @@ def validate(model, test_dl, loss_func):
             Y_pred = model(X)
             loss = loss_func(Y_pred, Y_true)
             test_loss += loss.item()
-            _, Y_pred = Y_pred.max(1)
-            correct += (Y_pred == Y_true).type(torch.DoubleTensor).sum().item()
+            X, Y_true, Y_pred = to_cpu(X, Y_true, Y_pred)
+            _, Y_pred = Y_pred.max(dim=1)
+            correct += (Y_pred == Y_true).type(torch.IntTensor).sum().item()
             total += len(Y_true)
             accs.append(correct/total)
     return accs
@@ -162,18 +171,19 @@ train_accs = []
 test_accs = []
 start_t = time.time()
 for epoch in range(epochs):
-    # train loop
+    # Train
     train_acc = train(model, train_dl, loss_func, opt)
     train_accs += train_acc
     t = time.time() - start_t
-    if verbose: print(epoch, int(t), "s , train_acc:", train_acc[-1])
+    if verbose: print(epoch, int(t), "s, train_acc:", train_acc[-1])
     
-    # validation loop
+    # Validate
     test_acc = validate(model, test_dl, loss_func)
     test_accs += test_acc
     t = time.time() - start_t
     if verbose: print(epoch, int(t), "s, test_acc:", test_acc[-1])
 
+# Store Accuracies
 tr_acc_df = pd.DataFrame(train_accs)
 tst_acc_df = pd.DataFrame(test_accs)
 
